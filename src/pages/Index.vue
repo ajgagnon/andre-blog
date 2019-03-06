96def46618224fd7ab8dd7c4c4e22804be91e209
Andre Gagnon <ajgagnon@uwalumni.com>
2019-03-06 08:20:15 -0600

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,7 +4,7 @@
       <header>
         <div class="container xl:max-w-xl mx-auto text-center px-6 py-10 md:py-32 border-b border-grey-lighter">
           <h1 class="text-4xl sm:text-5xl md:text-6xl font-sans font-bold mb-1">
-            <g-link to="/" class="text-black no-underline">Bleda</g-link>
+            <g-link to="/" class="text-black no-underline">Andre Gagnon</g-link>
           </h1>
           <p class="text-grey-dark text-lg sm:text-3xl">Thoughts, stories, and ideas.</p>
         </div>
